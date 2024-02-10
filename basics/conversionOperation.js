72daa1378b81ca3a46bc7881b0b6bedb934c78d2
vvndsz <148690245+vvndsz@users.noreply.github.com>
2024-02-10 16:45:03 +0000

    updated 3rd commit

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -18,3 +18,29 @@ let snum = String(num)
 console.log(snum); 
 
 console.log(typeof snum); 
+
+
+let val=3
+let negval= -val
+console.log(negval);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2%3);
+
+let str1 = "hello"
+let str2 = " world"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(+true);
+
+let counter = 100
+counter++;
+console.log(counter);
