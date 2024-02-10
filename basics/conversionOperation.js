da1b7b28831803b5ce1348fb7349bd92c0b86db7
vvndsz <148690245+vvndsz@users.noreply.github.com>
2024-02-10 16:30:36 +0000

    3rd commit

---
+++ basics/conversionOperation.js
@@ -0,0 +1,20 @@
+let score = true
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+let isLogin = null
+
+let booleanIsLogin =Boolean(isLogin)
+console.log(booleanIsLogin);
+
+let num=10
+let snum = String(num)
+console.log(snum); 
+
+console.log(typeof snum); 
