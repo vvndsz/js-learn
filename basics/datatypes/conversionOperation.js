52993a6ef24ba7fc923f141d1def4e800ec2bc0c
vvndsz <148690245+vvndsz@users.noreply.github.com>
2024-02-10 17:10:55 +0000

    Datatypes

---
+++ basics/datatypes/conversionOperation.js
@@ -0,0 +1,46 @@
+let score = true
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+let isLogin = null
+
+let booleanIsLogin =Boolean(isLogin)
+console.log(booleanIsLogin);
+
+let num=10
+let snum = String(num)
+console.log(snum); 
+
+console.log(typeof snum); 
+
+
+let val=3
+let negval= -val
+console.log(negval);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2%3);
+
+let str1 = "hello"
+let str2 = " world"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(+true);
+
+let counter = 100
+counter++;
+console.log(counter);
---
+++ basics/datatypes/summmary.js
@@ -0,0 +1,25 @@
+/*
+Primitive:
+String
+Number
+Boolean
+null
+undefined
+Symbol
+BigInt
+
+
+Reference(Non Premitive):
+Array
+Objects
+Functions
+*/
+
+const id = Symbol('123');
+const id_2 = Symbol('123');
+
+console.log(id === id_2);
+
+log.console(typeof id)
+
+
