7c9d9d4182ceaec575fdb46627e06850ba3ac209
vvndsz <148690245+vvndsz@users.noreply.github.com>
2024-02-10 05:16:32 +0000

    2nd commit

---
+++ basics/var.js
@@ -0,0 +1,11 @@
+const accId = 12345
+var accPass = "00000"
+let accEmail = "abc@mail.com"
+accCity = "Bangalore"
+let accState
+
+console.table([accEmail,accId,accPass, accCity, accState])
+
+/*
+var has issue in block and functional scope
+*/
