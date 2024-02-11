09c89f1dc76928def7210ebc5bdb187fe0c2b687
vvndsz <148690245+vvndsz@users.noreply.github.com>
2024-02-11 06:57:12 +0000

    6th commit

---
+++ basics/datatypes/strings.js
@@ -0,0 +1,28 @@
+const name = "abc"
+const repoCount = 10
+
+//console.log(name+ repoCount+" Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+// const gameName = new String('GameName')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+// const newString = gameName.substring(0,4)
+// console.log(newString);
+
+const string1 = "  name   "
+console.log(string1);
+console.log(string1.trim());
+
+const url = "https://google.com%20"
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('google'))
+
+console.log(url.split('/'))
