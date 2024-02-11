e61b926c3c7bdafd295e0fd6a6d2d77ea67bcf69
vvndsz <148690245+vvndsz@users.noreply.github.com>
2024-02-11 02:36:07 +0000

    6th commit

--- basics/datatypes/summmary.js
+++ basics/datatypes/summmary.js
@@ -15,11 +15,35 @@ Objects
 Functions
 */
 
-const id = Symbol('123');
-const id_2 = Symbol('123');
+// const id = Symbol('123');
+// const id_2 = Symbol('123');
 
-console.log(id === id_2);
+// console.log(id === id_2);
 
-log.console(typeof id)
+// log.console(typeof id)
 
+/////////////////////////////////////////////////
 
+// Stack-Primitive and
+// Heap-Reference Memory
+
+// let name="abc"
+
+// let name2 = name
+
+// name2 = "def"
+
+// console.log(name);
+// console.log(name2);
+
+// let user = {
+//     email:"user@mail.com",
+//     upi:"user@ybl"
+// }
+
+// let user2 = user
+
+// user2.email = "user@ymail.com"
+
+// console.log(user.email);
+// console.log(user2.email);
